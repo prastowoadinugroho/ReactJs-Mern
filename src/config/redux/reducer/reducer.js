4fb0c162f8f12d55246a92ea04b78f5e8815a2ce
PrastowoAdi <prastowoadinugroho@gmail.com>
2021-02-10 05:53:05 +0700

    Refactoring Create Blog Post
    Add redux to refactoring function create blog post

--- src/config/redux/reducer/reducer.js
+++ src/config/redux/reducer/reducer.js
@@ -1,9 +1,10 @@
 import {combineReducers} from 'redux';
 import globalReducer from './globalReducer';
 import homeReducer from './homeReducer';
+import createBlogReducer from './createBlogReducer';
 //untuk mengirimkan value atau state(global)
 //state adalah value state global
 //default value initialState
-const reducer = combineReducers({homeReducer, globalReducer})
+const reducer = combineReducers({homeReducer, globalReducer,createBlogReducer})
 
 export default reducer;
--- src/pages/CreateBlog/index.js
+++ src/pages/CreateBlog/index.js
@@ -2,42 +2,23 @@ import React, { useState } from 'react';
 import { Button, Gap, Input, Link, TextArea, Upload } from '../../components';
 import './createBlog.scss';
 import {useHistory} from 'react-router-dom';
-import Axios from 'axios';
+import { useDispatch, useSelector } from 'react-redux';
+import { postToAPI, setForm, setImgPreview } from '../../config/redux/action';
 
 const CreateBlog = () => {
-    const [title, setTitle] = useState('');
-    const [body, setBody] = useState('');
-    const [image, setImage] = useState('');
-    const [imagePreview, setImagePriview] = useState('');
+    const {form, imgPreview} = useSelector(state => state.createBlogReducer);
+    const {title, body} = form;
+    const dispatch = useDispatch();
     const history = useHistory();
 
     const onSubmit = () => {
-        console.log("title: ", title);
-        console.log("body: ", body);
-        console.log("image: ", image);
-
-        const data = new FormData();
-        data.append('title', title);
-        data.append('body', body);
-        data.append('image', image);
-
-        Axios.post('http://localhost:4000/v1/blog/post', data, {
-            headers: {
-                'content-type' : 'multipart/form-data'
-            }
-        })
-        .then(res => {
-            console.log('post success: ', res);
-        })
-        .catch(err => {
-            console.log('err: ', err);
-        })
+        postToAPI(form);
     }
 
     const onImageUpload = (e) => {
         const file = e.target.files[0];
-        setImage(file)
-        setImagePriview(URL.createObjectURL(file));
+        dispatch(setForm('image',file));
+        dispatch(setImgPreview(URL.createObjectURL(file)));
     }
     return (
         <div className="blog-post">
@@ -46,15 +27,15 @@ const CreateBlog = () => {
             <Input 
             label="Post Title"
             value={title}
-            onChange={ (e) => setTitle(e.target.value)}
+            onChange={ (e) => dispatch(setForm('title', e.target.value))}
             />
             <Upload
                 onChange={(e) => onImageUpload(e)}
-                img={imagePreview}
+                img={imgPreview}
             />
             <TextArea
                 value={body}
-                onChange={(e) => setBody(e.target.value)}
+                onChange={(e) => dispatch(setForm('body', e.target.value))}
             />
             <Gap height={20}/>
             <div className="button-action">
